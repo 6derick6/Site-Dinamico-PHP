0b79ea9a294dfbaeabea1f6b9313fafd074ae1a9
Dérick Trennepohl <6derick6@gmail.com>
2024-01-09 13:02:07 -0300

    base do projeto em html

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Site Dinâmico</title>
+    <meta name="keywords" content="site,dinamico,projeto">
+    <meta name="description" content="Descrição do meu site">
+</head>
+<body>
+    <header>
+        <div class="center">
+            <div class="logo">Logomarca</div>
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div>    
+    </header>
+
+    <section class="banner-principal">
+        <div class="center">
+            <form>
+                <h2>Qual seu melhor email?</h2>
+                <input type="email" name="email" required />
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </div>    
+    </section>
+
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50">
+                <h2>Dérick Trennepohl</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui. Sed dignissim eros elit, id elementum augue sodales sit amet. Aenean congue lacus non erat cursus viverra. Curabitur semper tellus nec euismod finibus. Sed rutrum vel urna tincidunt ornare. Praesent vel nunc dignissim, varius lorem ut, lobortis elit. Donec vitae odio non nibh malesuada suscipit. Mauris eleifend vel ex eu ornare. Donec in nisi quis dolor luctus tincidunt vitae quis purus. Nunc elementum arcu tellus, id euismod justo vehicula et. Donec venenatis, dolor ac pretium ullamcorper, massa mi ultrices sapien, eget cursus sapien augue a erat. Pellentesque lacinia tincidunt metus ac vulputate. Suspendisse molestie, ipsum nec malesuada laoreet, libero mi sodales felis, nec sagittis augue mi vel tellus. Aliquam erat volutpat. Nulla vulputate lectus in turpis consectetur aliquet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui. Sed dignissim eros elit, id elementum augue sodales sit amet. Aenean congue lacus non erat cursus viverra. Curabitur semper tellus nec euismod finibus. Sed rutrum vel urna tincidunt ornare. Praesent vel nunc dignissim, varius lorem ut, lobortis elit. Donec vitae odio non nibh malesuada suscipit. Mauris eleifend vel ex eu ornare. Donec in nisi quis dolor luctus tincidunt vitae quis purus. Nunc elementum arcu tellus, id euismod justo vehicula et. Donec venenatis, dolor ac pretium ullamcorper, massa mi ultrices sapien, eget cursus sapien augue a erat. Pellentesque lacinia tincidunt metus ac vulputate. Suspendisse molestie, ipsum nec malesuada laoreet, libero mi sodales felis, nec sagittis augue mi vel tellus. Aliquam erat volutpat. Nulla vulputate lectus in turpis consectetur aliquet.</p>
+            </div>
+        </div>        
+        <div class="w50">
+            <img src="images/foto.jfif" />
+        </div>
+    </section>
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box-especialidade">
+                <h3>icone 1</h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget</p>
+            </div>
+            <div class="box-especialidade">
+                <h3>icone 2</h3>
+                <h3>HTML</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget</p>
+            </div>
+            <div class="box-especialidade">
+                <h3>icone 3</h3>
+                <h3>JAVASCRIPT</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="extras">
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+            </div>
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a odio laoreet, tristique quam eget, euismod dui.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="center">
+            <p>Todos os diretos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
